1f9513ed77c900fa561f58646e6257c83f2c78a2
Jmackas <jamesrossmackay@gmail.com>
2017-09-17 09:09:29 +0800

    Update

--- Code Dumps.cs
+++ Code Dumps.cs
@@ -11,7 +11,33 @@ Try catch block
 	
 // Example
 	try {
-	
+		decimal paintArea, paintPrice;
 	} catch {
-	 MessageBox.Show("Code Dump");
+	 MessageBox.Show(/* Code */);
 	}
+	
+	
+	
+/****************************************
+If-else statement
+****************************************/
+// Syntax
+if (/* Statement */) {
+	// display code
+}
+
+
+// Example
+if ( x = 1 ) {
+ MessageBox.Show("Please enter a realistic speed.");
+}
+					
+					
+					
+					
+					
+					
+					
+					
+					
+					
