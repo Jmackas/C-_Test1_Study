d7cba46d515e7a9c822751f042ae6ef8ffd7c7b8
Jmackas <jamesrossmackay@gmail.com>
2017-09-17 17:53:57 +0800

    Update

---
+++ 1 - Main/Code Snippets.cs
@@ -0,0 +1,19 @@
+/****************************************
+Parsing string to data type
+****************************************/
+paintArea = decimal.Parse(inputPaintArea.Text);
+
+ /****************************************
+Outputting label contents
+****************************************/	
+// Outputing 
+outputPaintCost.Text = paintCost.ToString();
+
+// Outputing as a number
+outputPaintCost.Text = paintCost.ToString("c");
+
+/****************************************
+Closing form button
+****************************************/
+ this.Close();
+
---
+++ 1 - Main/Coding Structures.cs
@@ -0,0 +1,31 @@
+/****************************************
+Try catch block 
+****************************************/
+try {
+ // all code between here
+} catch {
+ MessageBox.Show("Code Dump");
+}
+
+/****************************************
+If statement
+****************************************/
+if (x = 1) {
+ MessageBox.Show("Please enter a realistic speed.");
+}
+
+/****************************************
+If-else statement
+****************************************/
+if (x = 1) {
+ MessageBox.Show("True");
+} else {
+ MessageBox.Show("False");
+}
+
+/****************************************
+While loop 
+****************************************/
+while (n > 6) {
+ Console.WriteLine("current value is" + n);
+}
