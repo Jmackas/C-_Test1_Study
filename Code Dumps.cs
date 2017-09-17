420738cdb347c1b8f53b04c8ab5e40fa60b07f97
Jmackas <jamesrossmackay@gmail.com>
2017-09-17 09:04:03 +0800

    Update

---
+++ Code Dumps.cs
@@ -0,0 +1,17 @@
+
+/****************************************
+Try catch block 
+****************************************/
+// Syntax
+	try {
+	 // All code between here
+	} catch {
+	 MessageBox.Show("Code Dump");
+	}
+	
+// Example
+	try {
+	
+	} catch {
+	 MessageBox.Show("Code Dump");
+	}
